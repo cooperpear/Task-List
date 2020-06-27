fe69ccc0333d0229765cc530ec4bf240d0f79ad4
Cooper Pearson <mptprogesterone@gmail.com>
2020-06-27 12:42:11 -0500

    script

--- app.js
+++ app.js
@@ -4,3 +4,38 @@ const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
+
+//Load all event listeners function call
+loadEventListeners();
+
+//Load all event listeners function creation
+function loadEventListeners() {
+    //Add to task events
+    form.addEventListener('submit', addTask);
+}
+
+//Add Task function
+function addTask(e) {
+    //If value left empty, propmt to add a task
+    if (taskInput.value === '') {
+        alert('Add a task');
+    }
+
+    //Create li element
+    const li = document.createElement('li');
+    //Assign a class name
+    li.className = 'collection-item';
+    //Create text node and append to li
+    li.appendChild(document.createTextNode(taskInput.value));
+    //Create new link element
+    const link = document.createElement('a');
+    //Assign a class name 
+    li.link = 'delete-item secondary-content';
+    //Create icon html
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+
+
+
+    //prevent default action of "form submit"
+    e.preventDefault();
+}
