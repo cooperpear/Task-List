9ed9dc90e8a95f84fdf5de77794cf3bc91b7ab28
Cooper Pearson <mptprogesterone@gmail.com>
2020-06-27 13:01:40 -0500

    debug. clean code

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ loadEventListeners();
 function loadEventListeners() {
     //Add to task events
     form.addEventListener('submit', addTask);
+    //Remove task event
+    taskList.addEventListener('click', removeTask);
+    //Clear task event
+    clearBtn.addEventListener('click', clearTasks);
 }
 
 //Add Task function
@@ -20,6 +24,7 @@ function addTask(e) {
     if (taskInput.value === '') {
         alert('Add a task');
     }
+
     //Create li element
     const li = document.createElement('li');
     //Assign a class name
@@ -42,4 +47,16 @@ function addTask(e) {
 
     //prevent default action of "form submit"
     e.preventDefault();
+}
+
+//Create Remove task function
+function removeTask(e) {
+    //Check to see if parent element ('a' tag) contains a class of "delete-item"
+    if (e.target.parentElement.classList.contains
+        ('delete-item')) {
+        //IF true, THEN run confirm statement and IF true, THEN remove element
+        if (confirm('Are You Sure?')) {
+            e.target.parentElement.parentElement.remove();
+        }
+    }
 }
